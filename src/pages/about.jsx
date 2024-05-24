03773353fab5d10d51cb39580baba4a422d30e3a
nerahubswebdev <nerahubwebdevelopment@gmail.com>
2024-05-24 10:25:17 +0100

    added code

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/header";
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <p>About</p>
+    </div>
+  );
+};
+
+export default About;
