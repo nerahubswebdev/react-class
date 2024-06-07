1f5bb6242edf2466f722538f8a9a9db20301a3a6
nerahubswebdev <nerahubwebdevelopment@gmail.com>
2024-06-07 12:39:58 +0100

    update

--- src/pages/single-blog-post.jsx
+++ src/pages/single-blog-post.jsx
@@ -6,13 +6,14 @@ const SingleBlogPost = ({ cohort2post }) => {
 
   return (
     <Link to={`/blog/${cohort2post?.slug}`}>
-      <div className="border border-blue-300 drop-shadow-md rounded-md p-3">
-        <img src={cohort2post?.post_image} alt="img" />
+      <div className="border border-blue-300 drop-shadow-md rounded-md p-3 h-[550px]">
+        <img src={cohort2post?.post_image} alt="img" className="h-60 w-full" />
         <h5 className="text-green-500">{cohort2post?.title}</h5>
         <p
-          className="text-gray-600"
+          className="text-gray-600 h-56 overflow-hidden"
           dangerouslySetInnerHTML={{ __html: cohort2post?.post_content }}
         />
+        <button>readmore</button>
       </div>
     </Link>
   );
